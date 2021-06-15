01edb27f02639c9946a6f5356af64ef0ddf64ca3
unknown <alfahrimudzik@gmail.com>
2021-06-15 09:13:30 +0700

    adding navbar, floating search box

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const translate = document.querySelectorAll('.translate');
-const zoom = document.querySelectorAll('.zoom')
+const zoom = document.querySelectorAll('.zoom');
+const title = document.querySelector('.big-title');
 
 window.addEventListener('scroll', () => {
     let scroll = window.pageYOffset;
@@ -11,4 +12,5 @@ window.addEventListener('scroll', () => {
     zoom.forEach( element => {
         element.style.width = `${scroll/2 + 600}px`
     })
+    title.style.opacity = `${100/scroll}`
 })
