dd7cb5f4881018b346986e2f792f59eefc19d4f3
unknown <alfahrimudzik@gmail.com>
2021-06-16 12:03:01 +0700

    finishing

--- app.js
+++ app.js
@@ -6,11 +6,22 @@ window.addEventListener('scroll', () => {
     let scroll = window.pageYOffset;
     translate.forEach( element => {
         let speed = element.dataset.speed;
-        console.log(speed);
         element.style.transform = `translateY(${scroll * speed}px)`
     })
     zoom.forEach( element => {
         element.style.width = `${scroll/2 + 600}px`
     })
     title.style.opacity = `${100/scroll}`
-})
+});
+
+$(window).on("scroll", function(){
+    if ( $(window).scrollTop() ){
+        $('.navbar').css('background-color', 'rgba(0, 0, 0, 0.4)');
+    }
+    else {
+        $('.navbar').css('background-color', 'rgba(0, 0, 0, 0)');
+    }
+});
+
+AOS.init(
+);
